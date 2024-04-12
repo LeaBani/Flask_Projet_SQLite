a422fa7d5863debf74a6da64e9157d18bdb4e854
Lea Bani <113905453+LeaBani@users.noreply.github.com>
2024-04-12 14:31:30 +0200

    Create createdb2.py

---
+++ createdb2.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+# Connexion à la base de données SQLite
+connection = sqlite3.connect('bibliotheque.db')
+
+# Ouverture du fichier contenant le schéma SQL
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+# Création d'un curseur pour exécuter des requêtes SQL
+cur = connection.cursor()
+
+# Ajout des données d'exemple dans la table Utilisateurs
+cur.execute("INSERT INTO Utilisateurs (Nom, Prenom, Adresse_email, Mot_de_passe, Est_Administrateur) VALUES (?, ?, ?, ?, ?)",
+            ('Dupont', 'Emilie', 'emilie.dupont@example.com', 'motdepasse123', 0))
+cur.execute("INSERT INTO Utilisateurs (Nom, Prenom, Adresse_email, Mot_de_passe, Est_Administrateur) VALUES (?, ?, ?, ?, ?)",
+            ('Leroux', 'Lucas', 'lucas.leroux@example.com', 'motdepasse456', 0))
+cur.execute("INSERT INTO Utilisateurs (Nom, Prenom, Adresse_email, Mot_de_passe, Est_Administrateur) VALUES (?, ?, ?, ?, ?)",
+            ('Admin', 'Admin', 'admin@example.com', 'admin123', 1))
+
+# Ajout des données d'exemple dans la table Livres
+cur.execute("INSERT INTO Livres (Titre, Auteur, Annee_de_publication, Genre, Nombre_exemplaires_disponibles) VALUES (?, ?, ?, ?, ?)",
+            ('Harry Potter à l\'école des sorciers', 'J.K. Rowling', 1997, 'Fantasy', 5))
+cur.execute("INSERT INTO Livres (Titre, Auteur, Annee_de_publication, Genre, Nombre_exemplaires_disponibles) VALUES (?, ?, ?, ?, ?)",
+            ('Le Seigneur des Anneaux', 'J.R.R. Tolkien', 1954, 'Fantasy', 3))
+cur.execute("INSERT INTO Livres (Titre, Auteur, Annee_de_publication, Genre, Nombre_exemplaires_disponibles) VALUES (?, ?, ?, ?, ?)",
+            ('1984', 'George Orwell', 1949, 'Dystopie', 7))
+
+# Commit des changements et fermeture de la connexion
+connection.commit()
+connection.close()
